8be9d5ba9c3fe26d6c1e0c7583f49554b4e04961
5Art <asheanx@gmail.com>
2026-02-01 17:11:29 +0100

    2.0 beta

--- addons/flowerwall_postprocess/scripts/config ui scripts/fl_pp_presets.gd
+++ addons/flowerwall_postprocess/scripts/config ui scripts/fl_pp_presets.gd
@@ -1,20 +1,20 @@
 extends OptionButton
 
-@export var color_compression: HSlider
-@export var downscale: HSlider
-@export var preblur_sl: HSlider
-@export var grain_sl: HSlider
-@export var scanlines_size_slider: HSlider
-@export var scanlines_sl: HSlider
-@export var curve_sl: HSlider
-@export var vignette_size_sl: HSlider
-@export var vignette_smoothness_sl: HSlider
-@export var wiggle_sl: HSlider
-@export var chromaberr_sl: HSlider
-@export var brightness: HSlider
+@export var preblurx_sl: HSlider
+@export var preblury_sl: HSlider
 @export var bloom_t: HSlider
 @export var bloom_i: HSlider
-@export var rgb: CheckButton
+@export var smear_sl: HSlider
+@export var scanlines_size_sl: HSlider
+@export var scanlines_op_sl: HSlider
+@export var scanlines_int_sl: HSlider
+@export var grain_sl: HSlider
+@export var wiggle_sl: HSlider
+@export var curve_sl: HSlider
+@export var grid: CheckButton
+@export var slot: CheckButton
+@export var mask_strength_sl: HSlider
+@export var pixel_size_sl: HSlider
 
 func _ready() -> void:
 	connect("item_selected", _on_preset_selected)
@@ -22,104 +22,105 @@ func _ready() -> void:
 
 func _on_preset_selected(value:int) -> void:
 	match value:
-		0: # High Res
-			color_compression.value = 0
-			downscale.value = 1.0
-			preblur_sl.value = 0.0
-			grain_sl.value = 0.0
-			scanlines_sl.value = 0.0
-			scanlines_size_slider.value = 0.0
+		0: # Subtle Slot
+			preblurx_sl.value = 2.0
+			preblury_sl.value = 0.0
+			grain_sl.value = 0.05
+			scanlines_size_sl.value = 1.0
+			scanlines_op_sl.value = 0.25
+			scanlines_int_sl.value = 3.0
 			curve_sl.value = 1.0
-			vignette_size_sl.value = 0.1
-			vignette_smoothness_sl.value = 0.05
-			wiggle_sl.value = 0.0
-			chromaberr_sl.value = 0.0
-			brightness.value = 1.0
-			bloom_t.value = 0.0
-			bloom_i.value = 0.0
-			rgb.button_pressed = false
-
-		1: # (Dithered, pixelated)
-			color_compression.value = 3
-			downscale.value = 5.0
-			preblur_sl.value = 0.0
-			grain_sl.value = 0.0
-			scanlines_sl.value = 0.0
-			scanlines_size_slider.value = 0.0
+			wiggle_sl.value = 0.01
+			bloom_t.value = 0.05
+			bloom_i.value = 0.7
+			smear_sl.value = 0.2
+			slot.button_pressed = true
+			grid.button_pressed = false
+			mask_strength_sl.value = 0.4
+			pixel_size_sl.value = 3.0
+	
+		1: # Subtle Grid
+			preblurx_sl.value = 2.0
+			preblury_sl.value = 0.0
+			grain_sl.value = 0.05
+			scanlines_size_sl.value = 1.0
+			scanlines_op_sl.value = 0.25
+			scanlines_int_sl.value = 3.0
 			curve_sl.value = 1.0
-			vignette_size_sl.value = 0.1
-			vignette_smoothness_sl.value = 0.05
-			wiggle_sl.value = 0.0
-			chromaberr_sl.value = 0.0
-			brightness.value = 1.0
-			bloom_t.value = 0.0
-			bloom_i.value = 0.0
-			rgb.button_pressed = false
-
-		2: # Sharp CRT (CRT Monitor)
-			color_compression.value = 0
-			downscale.value = 1.0
-			preblur_sl.value = 2.0
-			grain_sl.value = 0.1
-			scanlines_sl.value = 0.6
-			scanlines_size_slider.value = 2.0
+			wiggle_sl.value = 0.01
+			bloom_t.value = 0.05
+			bloom_i.value = 0.7
+			smear_sl.value = 0.2
+			slot.button_pressed = false
+			grid.button_pressed = true
+			mask_strength_sl.value = 0.4
+			pixel_size_sl.value = 3.0
+	
+		2: # Medium slot
+			preblurx_sl.value = 3.0
+			preblury_sl.value = 0.0
+			grain_sl.value = 0.05
+			scanlines_size_sl.value = 2.0
+			scanlines_op_sl.value = 0.20
+			scanlines_int_sl.value = 3.0
+			curve_sl.value = 1.0
+			wiggle_sl.value = 0.01
+			bloom_t.value = 0.05
+			bloom_i.value = 0.7
+			smear_sl.value = 0.3
+			slot.button_pressed = true
+			grid.button_pressed = false
+			mask_strength_sl.value = 0.3
+			pixel_size_sl.value = 4.0
+	
+		3: # Medium grid
+			preblurx_sl.value = 3.0
+			preblury_sl.value = 0.0
+			grain_sl.value = 0.05
+			scanlines_size_sl.value = 2.0
+			scanlines_op_sl.value = 0.20
+			scanlines_int_sl.value = 3.0
+			curve_sl.value = 1.0
+			wiggle_sl.value = 0.01
+			bloom_t.value = 0.05
+			bloom_i.value = 0.7
+			smear_sl.value = 0.3
+			slot.button_pressed = false
+			grid.button_pressed = true
+			mask_strength_sl.value = 0.3
+			pixel_size_sl.value = 4.0
+	
+		4: # VHS
+			preblurx_sl.value = 4.0
+			preblury_sl.value = 2.0
+			grain_sl.value = 0.35
+			scanlines_size_sl.value = 3.0
+			scanlines_op_sl.value = 0.2
+			scanlines_int_sl.value = 6.0
+			curve_sl.value = 1.025
+			wiggle_sl.value = 0.05
+			bloom_t.value = 0.05
+			bloom_i.value = 0.6
+			smear_sl.value = 0.6
+			slot.button_pressed = false
+			grid.button_pressed = false
+			mask_strength_sl.value = 0.0
+			pixel_size_sl.value = 3.0
+	
+		5: # Disabled
+			preblurx_sl.value = 0.0
+			preblury_sl.value = 0.0
+			grain_sl.value = 0.0
+			scanlines_size_sl.value = 0.0
+			scanlines_op_sl.value = 0.0
+			scanlines_int_sl.value = 0.0
 			curve_sl.value = 1.00
-			vignette_size_sl.value = 0.1
-			vignette_smoothness_sl.value = 0.05
 			wiggle_sl.value = 0.00
-			chromaberr_sl.value = 0.0
-			brightness.value = 1.2
-			bloom_t.value = 0.14
-			bloom_i.value = 2.0
-			rgb.button_pressed = true
-
-		3: # CRT TV (Blurry, Noisy)
-			color_compression.value = 0
-			downscale.value = 1.0
-			preblur_sl.value = 5.0
-			grain_sl.value = 0.25
-			scanlines_sl.value = 0.45
-			scanlines_size_slider.value = 1.0
-			curve_sl.value = 1.03
-			vignette_size_sl.value = 0.4
-			vignette_smoothness_sl.value = 0.4
-			wiggle_sl.value = 0.02
-			chromaberr_sl.value = 1.0
-			brightness.value = 1.3
-			bloom_t.value = 0.2
-			bloom_i.value = 1.9
-			rgb.button_pressed = true
-
-		4: # VHS Tape
-			color_compression.value = 3
-			downscale.value = 2.0
-			preblur_sl.value = 4
-			grain_sl.value = 0.4
-			scanlines_sl.value = 0.15
-			scanlines_size_slider.value = 4.0
-			curve_sl.value = 1.0
-			vignette_size_sl.value = 0.1
-			vignette_smoothness_sl.value = 0.05
-			wiggle_sl.value = 0.07
-			chromaberr_sl.value = 4
-			brightness.value = 1.0
-			bloom_t.value = 0.14
-			bloom_i.value = 0.1
-			rgb.button_pressed = false
-
-		5: #Artoria's prefered preset
-			color_compression.value = 0
-			downscale.value = 0.0
-			preblur_sl.value = 3.0
-			grain_sl.value = 0.2
-			scanlines_sl.value = 0.4
-			scanlines_size_slider.value = 3.0
-			curve_sl.value = 1.0
-			vignette_size_sl.value = 0.1
-			vignette_smoothness_sl.value = 0.05
-			wiggle_sl.value = 0.00
-			chromaberr_sl.value = 0.0
-			brightness.value = 1.2
-			bloom_t.value = 0.16
-			bloom_i.value = 1.9
-			rgb.button_pressed = true
+			bloom_t.value = 0.0
+			bloom_i.value = 0.0
+			smear_sl.value = 0.0
+			slot.button_pressed = false
+			grid.button_pressed = false
+			mask_strength_sl.value = 0.0
+			pixel_size_sl.value = 3.0
+			
--- addons/flowerwall_postprocess/scripts/flowerwall_crt_script.gd
+++ addons/flowerwall_postprocess/scripts/flowerwall_crt_script.gd
@@ -1,12 +1,14 @@
-class_name flowerwallPP
+class_name flowerwallCRT
 extends Control
 
 @export var flowerwall_crt_config_ui: CanvasLayer
-@export var dither: CanvasLayer
 @export var preblur_x: CanvasLayer
 @export var preblur_y: CanvasLayer
 @export var post_bloom: CanvasLayer
 @export var crt: CanvasLayer
+
+@export var grid_toggle: CheckButton
+@export var slot_toggle: CheckButton
 
 const BLOOM_SHADER = preload("res://addons/flowerwall_postprocess/shaders/bloom_shader.material")
 const BLURX_SHADER = preload("res://addons/flowerwall_postprocess/shaders/blurx_shader.material")
@@ -15,7 +17,6 @@ const CRT_SHADER = preload("res://addons/flowerwall_postprocess/shaders/crt_shad
 const DITHERING_SHADER = preload("res://addons/flowerwall_postprocess/shaders/dithering_shader.material")
 
 func _ready() -> void:
-	should_enable_dither()
 	should_enable_blur()
 	should_enable_crt()
 	should_enable_bloom()
@@ -27,22 +28,15 @@ func _unhandled_key_input(event: InputEvent) -> void:
 		if event.pressed and event.keycode == KEY_F1:
 			flowerwall_crt_config_ui.visible = !flowerwall_crt_config_ui.visible
 
-func should_enable_dither() -> void:
-	if DITHERING_SHADER.get("shader_parameter/downscaling_strength") > 1.0: 
-		dither.visible = true
-		return
-	if DITHERING_SHADER.get("shader_parameter/color_compression") > 0.0:
-		dither.visible = true
-		return
-	dither.visible = false
-
 func should_enable_blur() -> void:
 	if BLURX_SHADER.get("shader_parameter/radius") > 0.0:
 		preblur_x.visible = true
+	else: 
+		preblur_x.visible = false
+	if BLURY_SHADER.get("shader_parameter/radius") > 0.0:
 		preblur_y.visible = true
-		return
-	preblur_x.visible = false
-	preblur_y.visible = false
+	else: 
+		preblur_y.visible = false
 
 func should_enable_crt() -> void:
 	crt.visible = false
@@ -52,17 +46,17 @@ func should_enable_crt() -> void:
 	else:
 		CRT_SHADER.set("shader_parameter/enable_grain", false)
 
-	if CRT_SHADER.get("shader_parameter/scanline_opacity") > 0.0:
+	if CRT_SHADER.get("shader_parameter/scanlines_opacity") > 0.0:
 		CRT_SHADER.set("shader_parameter/enable_scanlines", true)
 		crt.visible = true
 	else:
 		CRT_SHADER.set("shader_parameter/enable_scanlines", false)
 
 	if CRT_SHADER.get("shader_parameter/wiggle") > 0.0:
-		CRT_SHADER.set("shader_parameter/enable_vhs_wiggle", true)
+		CRT_SHADER.set("shader_parameter/enable_wiggle", true)
 		crt.visible = true
 	else:
-		CRT_SHADER.set("shader_parameter/enable_vhs_wiggle", false)
+		CRT_SHADER.set("shader_parameter/enable_wiggle", false)
 
 	if CRT_SHADER.get("shader_parameter/curve_power") > 1.0:
 		CRT_SHADER.set("shader_parameter/enable_curving", true)
@@ -90,43 +84,41 @@ func should_enable_bloom() -> void:
 
 #region Signals
 
-func _on_downscale_slider_value_changed(value: float) -> void:
-	DITHERING_SHADER.set("shader_parameter/downscaling_strength", value)
-	should_enable_dither()
 
-
-func _on_color_depth_slider_value_changed(value: float) -> void:
-	DITHERING_SHADER.set("shader_parameter/color_compression", value)
-	should_enable_dither()
-
-
-func _on_blur_slider_value_changed(value: float) -> void:
-	BLURY_SHADER.set("shader_parameter/radius", value)
+func _on_blurx_slider_value_changed(value: float) -> void:
 	BLURX_SHADER.set("shader_parameter/radius", value)
 	should_enable_blur()
 
-
-func _on_brightness_modifier_slider_value_changed(value: float) -> void:
-	CRT_SHADER.set("shader_parameter/brightness_multiplier", value)
-
+func _on_blury_slider_value_changed(value: float) -> void:
+	BLURY_SHADER.set("shader_parameter/radius", value)
+	should_enable_blur()
 
 func _on_bloom_intensity_slider_value_changed(value: float) -> void:
 	BLOOM_SHADER.set("shader_parameter/bloom_intensity", value)
 	should_enable_bloom()
-
 
 func _on_bloom_threshold_slider_value_changed(value: float) -> void:
 	BLOOM_SHADER.set("shader_parameter/bloom_threshold", value)
 	should_enable_bloom()
 
 
-func _on_chroma_aberr_strength_slider_value_changed(value: float) -> void:
+func _on_smearing_strength_slider_value_changed(value: float) -> void:
 	CRT_SHADER.set("shader_parameter/smearing_strength", value)
 	should_enable_crt()
 
 
-func _on_rgb_grid_toggle_toggled(toggled_on: bool) -> void:
-	CRT_SHADER.set("shader_parameter/enable_rgb_grid", toggled_on)
+func _on_gridmask_toggle_toggled(toggled_on: bool) -> void:
+	CRT_SHADER.set("shader_parameter/enable_gridmask", toggled_on)
+	if slot_toggle.toggled:
+		slot_toggle.set_pressed_no_signal(false)
+		CRT_SHADER.set("shader_parameter/enable_slotmask", false)
+	should_enable_crt()
+
+func _on_slotmask_toggle_toggled(toggled_on: bool) -> void:
+	CRT_SHADER.set("shader_parameter/enable_slotmask", toggled_on)
+	if grid_toggle.toggled:
+		grid_toggle.set_pressed_no_signal(false)
+		CRT_SHADER.set("shader_parameter/enable_gridmask", false)
 	should_enable_crt()
 
 
@@ -136,7 +128,10 @@ func _on_scanlines_opacity_slider_value_changed(value: float) -> void:
 
 func _on_scanlines_size_slider_value_changed(value: float) -> void:
 	CRT_SHADER.set("shader_parameter/scanlines_thickness", value)
-	CRT_SHADER.set("shader_parameter/scanlines_interval", value *2)
+	should_enable_crt()
+	
+func _on_scanlines_interval_slider_value_changed(value: float) -> void:
+	CRT_SHADER.set("shader_parameter/scanlines_interval", value)
 	should_enable_crt()
 
 func _on_grain_slider_value_changed(value: float) -> void:
@@ -144,18 +139,8 @@ func _on_grain_slider_value_changed(value: float) -> void:
 	should_enable_crt()
 
 
-func _on_vhs_wiggle_strength_slider_value_changed(value: float) -> void:
+func _on_wiggle_strength_slider_value_changed(value: float) -> void:
 	CRT_SHADER.set("shader_parameter/wiggle", value)
-	should_enable_crt()
-
-
-func _on_vignette_size_slider_value_changed(value: float) -> void:
-	CRT_SHADER.set("shader_parameter/vignette_size", value)
-	should_enable_crt()
-
-
-func _on_vignette_smoothness_slider_value_changed(value: float) -> void:
-	CRT_SHADER.set("shader_parameter/vignette_smoothness", value)
 	should_enable_crt()
 
 
@@ -163,4 +148,11 @@ func _on_curvature_slider_value_changed(value: float) -> void:
 	CRT_SHADER.set("shader_parameter/curve_power", value)
 	should_enable_crt()
 
+func _on_mask_strength_slider_value_changed(value: float) -> void:
+	CRT_SHADER.set("shader_parameter/mask_strength", value)
+	should_enable_crt()
+
+func _on_pixel_size_slider_value_changed(value: float) -> void:
+	CRT_SHADER.set("shader_parameter/pixel_size", value)
+	should_enable_crt()
 #endregion
